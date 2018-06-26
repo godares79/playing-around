856b05cb00d2f075efcc9fa69815916e49b04421
David Dietrich <david.drich@gmail.com>
2018-06-25 23:13:25 -0700

    Some iterative tree traversals

--- src/datastructures/BinaryTree.java
+++ src/datastructures/BinaryTree.java
@@ -1,7 +1,8 @@
 package datastructures;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.*;
 
 /**
  * Very simple binary tree.
@@ -31,7 +32,7 @@ public class BinaryTree {
     }
 
     public List<Integer> inOrderTraversal() {
-        // Print the contents of the tree in order.
+        // Print the contents of the tree in order: left -> root -> right
         List<Integer> traversal = new ArrayList<>();
         inOrderTraversalHelper_Recursive(root, traversal);
         return traversal;
@@ -45,7 +46,7 @@ public class BinaryTree {
     }
 
     public List<Integer> postOrderTraversal() {
-        // Print the contents of the tree post order.
+        // Print the contents of the tree post order: left -> right -> root
         List<Integer> traversal = new ArrayList<>();
         postOrderTraversalHelper_Recursive(root, traversal);
         return traversal;
@@ -59,7 +60,7 @@ public class BinaryTree {
     }
 
     public List<Integer> preOrderTraversal() {
-        // Print the contents of the tree pre order.
+        // Print the contents of the tree pre order: root -> left -> right
         List<Integer> traversal = new ArrayList<>();
         preOrderTraversalHelper_Recursive(root, traversal);
         return traversal;
@@ -70,5 +71,112 @@ public class BinaryTree {
         traversal.add(root.data);
         preOrderTraversalHelper_Recursive(root.left, traversal);
         preOrderTraversalHelper_Recursive(root.right, traversal);
+    }
+
+    public List<Integer> inOrderTraversal_Iterative() {
+        Stack<Node> nodeStack = new Stack<>();
+        List<Integer> traversal = new ArrayList<>();
+
+        if (root == null) return traversal;
+        Node newRoot = root;
+        while (true) {
+            while (newRoot != null) {
+                nodeStack.push(newRoot);
+                newRoot = newRoot.left;
+            }
+            Node current = nodeStack.pop();
+            traversal.add(current.data);
+            if (current.right != null) {
+                newRoot = current.right;
+                continue;
+            }
+
+            if (nodeStack.isEmpty()) return traversal;
+        }
+    }
+
+    /*
+     * Just using a Stack and adding continues in the loop to emulate the recursion is not enough... nodes end up
+     * being readded in subsequent loops. However, adding a Set to hold the visited nodes is enough to get the
+     * algorithm working.
+     */
+    public List<Integer> postOrderTraversalHelper_Iterative_ExtraMemory() {
+        Stack<Node> nodeStack = new Stack<>();
+        Set<Node> visited = new HashSet<>();  // Just a regular HashSet is fine (vs. LinkedHashSet, TreeSet).
+        List<Integer> traversal = new ArrayList<>();
+
+        if (root == null) return traversal;
+        nodeStack.push(root);
+        while (true) {
+            if (nodeStack.peek().left != null && !visited.contains(nodeStack.peek().left)) {
+                visited.add(nodeStack.peek().left);
+                nodeStack.push(nodeStack.peek().left);
+                continue;
+            }
+            if (nodeStack.peek().right != null && !visited.contains(nodeStack.peek().right)) {
+                visited.add(nodeStack.peek().right);
+                nodeStack.push(nodeStack.peek().right);
+                continue;
+            }
+            traversal.add(nodeStack.pop().data);
+
+            if (nodeStack.isEmpty()) return traversal;
+        }
+    }
+
+    /*
+     * This implementation uses a single stack to hold state similar to the recursion stack.
+     * Then the tree root is changed in each pass of the algorithm and the postorder traversal
+     * essentially operates on subtrees until it needs to backtrack.
+     * It kind of reminds me of a DFS.
+     *
+     * I prefer the two data structures approach TBH. It is cleaner and easier to understand.
+     * The extra memory may be an issue on constrained devices or very large data sets, but that is
+     * rare compared to the typical case.
+     */
+    public List<Integer> postOrderTraversalHelper_Iterative_NoExtraMemory() {
+        Stack<Node> nodeStack = new Stack<>();
+        List<Integer> traversal = new ArrayList<>();
+
+        if (root == null) return traversal;
+        Node newRoot = root;
+        while (true) {
+            while (newRoot != null) {
+                nodeStack.push(newRoot);
+                if (newRoot.right != null) nodeStack.push(newRoot.right);
+                newRoot = newRoot.left;
+            }
+            Node current = nodeStack.pop();
+            // TODO: Checking the traversal data in this if statement is incorrect. The data may be duplicated.
+            // The proper answer there is to store the node refs in the traversal and not their data.
+            if (!nodeStack.isEmpty() &&
+                    nodeStack.peek().right == current &&
+                    (current.left != null || current.right != null) &&
+                    traversal.get(traversal.size()-1) != current.right.data) {
+                newRoot = current;
+                continue;
+            }
+            traversal.add(current.data);
+
+            if (nodeStack.isEmpty()) return traversal;
+        }
+    }
+
+    public List<Integer> preOrderTraversal_Iterative() {
+        Stack<Node> nodeStack = new Stack<>();
+        List<Integer> traversal = new ArrayList<>();
+
+        if (root == null) return traversal;
+        nodeStack.push(root);
+        while(!nodeStack.isEmpty()) {
+            Node current = nodeStack.pop();
+            if (current.right != null)
+                nodeStack.push(current.right);
+            if (current.left != null)
+                nodeStack.push(current.left);
+            traversal.add(current.data);
+        }
+
+        return traversal;
     }
 }
--- test/datastructures/BinaryTreeTest.java
+++ test/datastructures/BinaryTreeTest.java
@@ -3,7 +3,7 @@ package datastructures;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -13,7 +13,7 @@ class BinaryTreeTest {
 
     private static BinaryTree testTree = null;
 
-    @BeforeAll
+    @BeforeEach
     static void buildTestTree() {
         // 1 -> 2,3 -> 4,5,-,-
         testTree = new BinaryTree(1);
@@ -30,14 +30,48 @@ class BinaryTreeTest {
     }
 
     @Test
+    void inOrderTraversal_Iterative() {
+        testTree.root.right.left = new BinaryTree.Node(6);
+        testTree.root.right.right = new BinaryTree.Node(7);
+        testTree.root.left.right.left = new BinaryTree.Node(8);
+        List<Integer> traversal = testTree.inOrderTraversal_Iterative();
+        assertThat(traversal, is(Arrays.asList(4,2,8,5,1,6,3,7)));
+    }
+
+    @Test
     void postOrderTraversal() {
         List<Integer> traversal = testTree.postOrderTraversal();
         assertThat(traversal, is(Arrays.asList(4,5,2,3,1)));
     }
 
     @Test
+    void postOrderTraversal_Iterative_ExtraMemory() {
+        List<Integer> traversal = testTree.postOrderTraversalHelper_Iterative_ExtraMemory();
+        assertThat(traversal, is(Arrays.asList(4,5,2,3,1)));
+    }
+
+    @Test
+    void postOrderTraversal_Iterative_NoExtraMemory() {
+        testTree.root.right.left = new BinaryTree.Node(6);
+        testTree.root.right.right = new BinaryTree.Node(7);
+        List<Integer> traversal = testTree.postOrderTraversalHelper_Iterative_NoExtraMemory();
+        assertThat(traversal, is(Arrays.asList(4,5,2,6,7,3,1)));
+    }
+
+    @Test
     void preOrderTraversal() {
         List<Integer> traversal = testTree.preOrderTraversal();
         assertThat(traversal, is(Arrays.asList(1,2,4,5,3)));
+    }
+
+    @Test
+    void preOrderTraversal_Iterative() {
+        List<Integer> traversal = testTree.preOrderTraversal_Iterative();
+        assertThat(traversal, is(Arrays.asList(1,2,4,5,3)));
+
+        testTree.root.right.left = new BinaryTree.Node(6);
+        testTree.root.right.right = new BinaryTree.Node(7);
+        traversal = testTree.preOrderTraversal_Iterative();
+        assertThat(traversal, is(Arrays.asList(1,2,4,5,3,6,7)));
     }
 }
